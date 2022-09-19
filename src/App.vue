326da4b159ecac62bcf5ddff7c3f75507a6d744c
wangcb <1781998918@qq.com>
2022-09-19 19:40:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/vue.svg"/>
+  <Demo/>
+</template>
+
+<script>
+import Demo from "./components/buttonDemo.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {
+    Demo,
+  },
+});
+</script>
---
+++ src/components/buttonDemo.vue
@@ -0,0 +1,8 @@
+<template>
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
+</template>
+
