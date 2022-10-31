f4f4efc06f82fc1b9ee5cb5b468525054ea58d85
wangcb <1781998918@qq.com>
2022-10-31 11:17:46 +0800

    随机生成颜色工具

---
+++ src/render/utils/randomColor.ts
@@ -0,0 +1,9 @@
+export const randomColor = () => {
+    let color = "#";
+    let arr = ['6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    for (let i = 0; i < 3; i++) {
+        color += Math.floor(Math.random()*10)
+    }
+    console.log(color)
+    return color;
+}
